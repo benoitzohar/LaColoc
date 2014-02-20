4c0b79c3ea3450bee95b939ad639b134f93146f0
Benoit Zohar <benoit.zohar@versusmind.eu>
2014-02-20 15:59:07 +0100

    process.env logs

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ var express = require('express'),
     passportSocketIo = require("passport.socketio"),
     mongoose = require('mongoose'),
     mongoStore = require('connect-mongo')(express),
-    http = require('http')
+    http = require('http'),
+    os = require("os")
+
     
 console.log('process.env:',process.env);
+console.log('process.env.NODE_ENV',process.env.NODE_ENV);
+console.log('os.hostname();',os.hostname());
 // Load configurations
 var env = process.env.NODE_ENV || 'development'
   , config = require('./config/config')[env]
