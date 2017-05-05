64633d6a881616c8afc11b686bcf7a563b61b369
Benoit Zohar <benoit.zohar@gmail.com>
2017-05-04 20:19:30 -0400

    add no-semi to prettier + try to commit with fail test

--- server/core/api.test.js
+++ server/core/api.test.js
@@ -1,7 +1,7 @@
-import request from 'supertest-as-promised';
-import httpStatus from 'http-status';
-import chai, { expect } from 'chai';
-import app from '../../index';
+import request from 'supertest-as-promised'
+import httpStatus from 'http-status'
+import chai, { expect } from 'chai'
+import app from '../../index'
 
 describe('# Api and Routes', () => {
   describe('# GET /api/ping', () => {
@@ -10,12 +10,12 @@ describe('# Api and Routes', () => {
         .get('/api/ping')
         .expect(httpStatus.OK)
         .then(res => {
-          expect(res.text).to.equal('Pong!');
-          done();
+          expect(res.text).to.equal('Pongs!')
+          done()
         })
-        .catch(done);
-    });
-  });
+        .catch(done)
+    })
+  })
 
   describe('# GET /api/404', () => {
     it('should return 404 status', done => {
@@ -23,12 +23,12 @@ describe('# Api and Routes', () => {
         .get('/api/404')
         .expect(httpStatus.NOT_FOUND)
         .then(res => {
-          expect(res.body.message).to.equal('Not Found');
-          done();
+          expect(res.body.message).to.equal('Not Found')
+          done()
         })
-        .catch(done);
-    });
-  });
+        .catch(done)
+    })
+  })
 
   describe('# Error Handling', () => {
     it('should handle mongoose CastError - Cast to ObjectId failed', done => {
@@ -36,11 +36,11 @@ describe('# Api and Routes', () => {
         .get('/api/users/56z787zzz67fc')
         .expect(httpStatus.INTERNAL_SERVER_ERROR)
         .then(res => {
-          expect(res.body.message).to.equal('Internal Server Error');
-          done();
+          expect(res.body.message).to.equal('Internal Server Error')
+          done()
         })
-        .catch(done);
-    });
+        .catch(done)
+    })
 
     it('should handle express validation error - username is required', done => {
       request(app)
@@ -51,10 +51,10 @@ describe('# Api and Routes', () => {
         })
         .expect(httpStatus.BAD_REQUEST)
         .then(res => {
-          expect(res.body.message).to.equal('"name" is required');
-          done();
+          expect(res.body.message).to.equal('"name" is required')
+          done()
         })
-        .catch(done);
-    });
-  });
-});
+        .catch(done)
+    })
+  })
+})
